e44a2133d65574c62cb2e681a93faeb6ad3680f7
mdszamal <78942241+mdszamal@users.noreply.github.com>
2021-02-16 10:45:34 +1000

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,11 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    int_features= [float(x) for x in request.form.values()]
+    final_features= [np.array(int_features)]
+    prediction= model.predict(final_features)
+    output= round(prediction[0], 2)
+    return render_template("index.html", prediction_text= "flower is {}".format(output))
